a5917df2eec949df93ebc6cb7b183a445e0003c5
Rodney Osodo <socials@rodneyosodo.com>
2023-09-27 23:12:10 +0300

    feat(cli): add tests for CLI package
    This commit adds tests for the CLI package in order to ensure its functionality and reliability. The tests cover various scenarios and edge cases to validate the behavior of the CLI commands and options.
    
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

---
+++ cli/cli_test.go
@@ -0,0 +1,135 @@
+// Copyright (c) MpesaOverlay. All rights reserved.
+// Use of this source code is governed by a Apache-2.0 license that can be
+// found in the LICENSE file.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package cli
+
+import (
+	"testing"
+
+	"github.com/0x6flab/mpesaoverlay/pkg/mpesa"
+	"github.com/0x6flab/mpesaoverlay/pkg/mpesa/mocks"
+	"github.com/choria-io/fisk"
+)
+
+func TestToken(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	call := sdk.On("Token").Return(mpesa.TokenResp{}, nil)
+
+	err := Token(sdk)
+
+	call.Unset()
+
+	if err != nil {
+		t.Errorf("Token() error = %v", err)
+	}
+}
+
+func TestSTKPush(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	err := STKPush(sdk)
+
+	if err != nil {
+		t.Errorf("STKPush() error = %v", err)
+	}
+}
+
+func TestB2CPayment(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	err := B2CPayment(sdk)
+
+	if err != nil {
+		t.Errorf("B2CPayment() error = %v", err)
+	}
+}
+
+func TestAccountBalance(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	err := AccountBalance(sdk)
+
+	if err != nil {
+		t.Errorf("AccountBalance() error = %v", err)
+	}
+}
+
+func TestC2BRegisterURL(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	err := C2BRegisterURL(sdk)
+
+	if err != nil {
+		t.Errorf("C2BRegisterURL() error = %v", err)
+	}
+}
+
+func TestSTKPushQuery(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	err := STKPushQuery(sdk)
+
+	if err != nil {
+		t.Errorf("STKPushQuery() error = %v", err)
+	}
+}
+
+func TestC2BSimulate(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	err := C2BSimulate(sdk)
+
+	if err != nil {
+		t.Errorf("C2BSimulate() error = %v", err)
+	}
+}
+
+func TestQRGenerate(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	err := QRCode(sdk)
+
+	if err != nil {
+		t.Errorf("QRGenerate() error = %v", err)
+	}
+}
+
+func TestReversal(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	err := Reversal(sdk)
+
+	if err != nil {
+		t.Errorf("Reversal() error = %v", err)
+	}
+}
+
+func TestRemitTax(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	err := RemitTax(sdk)
+
+	if err != nil {
+		t.Errorf("RemitTax() error = %v", err)
+	}
+}
+
+func TestTransactionStatus(t *testing.T) {
+	sdk := new(mocks.SDK)
+
+	err := TransactionStatus(sdk)
+
+	if err != nil {
+		t.Errorf("TransactionStatus() error = %v", err)
+	}
+}
+
+func TestAddCommands(_ *testing.T) {
+	sdk := new(mocks.SDK)
+	app := fisk.New("mpesa-cli", "0.0.1")
+	AddCommands(app, sdk)
+}
